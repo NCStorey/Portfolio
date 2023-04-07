064ab487febcfc5aa9c17135d94e9d6f0066754d
NCStorey <ncsansom@hotmail.co.uk>
2023-04-07 16:47:03 +0100

    addition of framer motion

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Navbutton from './components/navbutton/navbutton';
 
 function App() {
   return (
-    <h1>
-      Nathalie Storey
-    </h1>
+    <>
+      <Navbutton />
+    </>
   );
 }
 
---
+++ src/components/navbutton/navbutton.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { motion } from "framer-motion"
+
+function Navbutton (){
+    return (
+        <>
+        <button>NavButton</button>
+        </>
+    )
+}
+
+export default Navbutton;
