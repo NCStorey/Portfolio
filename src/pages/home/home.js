70177c855020b321bd4f30d3ab16e253f04f80a3
NCStorey <ncsansom@hotmail.co.uk>
2023-04-23 20:56:46 +0100

    home page js and css

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './home.css'
+import profilePic from './charlotte-luke-hollyclarkphotography-h-905.jpg'
 
 
 function Home() {
@@ -8,13 +9,15 @@ function Home() {
       <div className='container-fluid home'>
         <div className='row homeflex'>
             <div className='col-12 hook'>
-                <span className='hookText'>Hey, are you new here?</span>
+              <span className='hookText'><span >Hey,</span><span> nice to see you</span></span>
             </div>
-            <div className='col-sm-12 col-md-4 col-lg-6 profilePicture'>
-
+            <div className='col-sm-12 col-md-4 col-lg-6 profilePictureCont'>
+              <span>
+                <img src={profilePic} alt='Profile Picture' className='profileImage'></img>
+              </span>
             </div>
             <div className='col-sm-12 col-md-8 col-lg-6 intro'>
-
+              <p>Firstly thank you for taking the time to visit. I am new to the world of front end development and still finding my way. I need to be creative. I started this journey in response to losing all of my free time to two little monkeys (also known as my children). '' </p>
             </div>
         </div>
       </div>
