1a435b54907a71ced57eb43b60ef03b64eff988d
NCStorey <ncsansom@hotmail.co.uk>
2023-05-01 20:21:55 +0100

    css add

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -20,10 +20,12 @@ function Home() {
           </div>
 
           <div className='intro'>
-            <p>Firstly thank you for taking the time to visit. I am new to the world of front end development and still finding my way.</p>
+            <p>Firstly thank you for taking the time to visit. I am new to the world of front end development and still finding my way. 
+            </p>
               
-            <span className='subtitle'>Why the change?</span> 
-            <p>I NEED to be creative. I need to be in an envorinment that allows a creative outlet. This is what is driving me to change and grow. In addition to this I thrive in an envorinment that fosters communication and teamwork. I enjoy learning and relish a challenge. Creativity, communication and continual learning are some of the cornerstones of web development and are what I need to be fulfilled.</p>
+            <span className='subtitle'>Why me?</span> 
+            <p>I NEED to be creative. I need to be in an envorinment that allows a creative outlet. This is what is driving me to change and grow. In addition to this I thrive in an envorinment that fosters communication and teamwork. I enjoy learning and relish a challenge. Creativity, communication and continual learning are some of the cornerstones of web development and are what I need to be fulfilled.
+            </p>
 
           </div>
 
